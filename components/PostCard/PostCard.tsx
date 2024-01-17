340184770abeef70ae29839b01965303a65c6193
Simeon Ikudabo <simeonikudabo@Simeons-Air.lan>
2024-01-17 18:05:49 -0500

    Adding the name section in the post card.

--- components/PostCard/PostCard.tsx
+++ components/PostCard/PostCard.tsx
@@ -1,5 +1,7 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import truncate from 'lodash/truncate';
 import { Surface } from "react-native-paper";
+import GeoCitiesAvatar from '../GeoCitiesAvatar';
 import GeoCitiesBodyText from '../GeoCitiesBodyText';
 import { colors } from '../colors';
 import { PostType } from '../../typings';
@@ -9,7 +11,9 @@ type DataLayerProps = {
 };
 
 type PostCardDisplayLayerProps = {
+    authorId: string;
     caption: string | undefined;
+    userName: string;
 };
 
 export default function PostCard({ post }: { post: PostType}) {
@@ -19,22 +23,29 @@ export default function PostCard({ post }: { post: PostType}) {
 }
 
 function PostCard_DisplayLayer({
+    authorId,
     caption,
+    userName,
 }: PostCardDisplayLayerProps) {
     return (
         <Surface style={styles.cardContainer}>
-            {caption && (
-                <GeoCitiesBodyText color={colors.white} text={caption} />
-            )}
+            <View style={styles.topCardSection}>
+                <GeoCitiesAvatar size={50} src={`${process.env.EXPO_PUBLIC_API_BASE_URI}get-photo-by-user-id/${authorId}`} />
+                <View style={styles.topSectionNameContainer}>
+                    <GeoCitiesBodyText color={colors.white} fontSize={14} fontWeight={900} text={userName} />
+                </View>
+            </View>
         </Surface>
     )
 }
 
 function useDataLayer({ post }: DataLayerProps) {
-    const { caption } = post;
+    const { authorId, caption, userName } = post;
     
     return {
+        authorId,
         caption,
+        userName: truncate(userName, { length: 30 }),
     };
 }
 
@@ -42,6 +53,19 @@ const styles = StyleSheet.create({
     cardContainer: {
         boarderRadius: 5,
         marginBottom: 20,
+    },
+    topCardSection: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        lineHeight: '1',
+        paddingLeft: 5,
+        paddingTop: 10,
+        width: '100%',
+    },
+    topSectionNameContainer: {
+        paddingLeft: 10,
+        paddingTop: 10,
     },
 });
 
