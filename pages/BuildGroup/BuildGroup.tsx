c25bf819ba67b35c09a12b431a6531368ab6045d
Simeon Ikudabo <simeonikudabo@Simeons-Air.lan>
2024-01-25 00:08:32 -0500

    Creating the general form container for the build group screen.

--- pages/BuildGroup/BuildGroup.tsx
+++ pages/BuildGroup/BuildGroup.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet, View } from 'react-native';
-import { useQueryClient } from '@tanstack/react-query'
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { useQueryClient } from '@tanstack/react-query';
+import { Surface } from 'react-native-paper';
 import { useUser } from '../../hooks/storage-hooks';
 import { GeoCitiesBodyText, colors } from '../../components';
 
@@ -12,6 +13,13 @@ function BuildGroup_DisplayLayer() {
         <View style={styles.container}>
             <View style={styles.topHeader}>
                 <GeoCitiesBodyText color={colors.white} fontSize={32} text='Create Group' />
+            </View>
+            <View style={styles.formContainer}>
+                <Surface elevation={4} style={styles.form}>
+                    <ScrollView>
+                        <GeoCitiesBodyText color={colors.white} text="Form" />
+                    </ScrollView>
+                </Surface>
             </View>
         </View>
     );
@@ -33,6 +41,18 @@ const styles = StyleSheet.create({
         height: '100%',
         paddingTop: 50,
         width: '100%',
+    },
+    form: {
+        height: '100%',
+        paddingLeft: 10,
+        paddingRight: 10,
+    },
+    formContainer: {
+        height: '100%',
+        paddingBottom: 100,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop: 30,
     },
     topHeader: {
         alignItems: 'center',
