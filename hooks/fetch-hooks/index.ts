51c727032b2404cbe0bca8cde0eeea3ac7a0559c
Simeon Ikudabo <simeonikudabo@Simeons-Air.lan>
2024-02-08 11:37:59 -0500

    Creating a hook to fetch a GeoGroup.

--- hooks/fetch-hooks/index.ts
+++ hooks/fetch-hooks/index.ts
@@ -1,10 +1,12 @@
 import { useFetchUserData } from "./useFetchUserData";
 import useFetchFeedPosts from "./useFetchFeedPosts";
+import useFetchGroup from "./useFetchGroup";
 import useFetchPost from "./useFetchPost";
 import useFetchUserProfilePosts from './useFetchUserProfilePosts';
 
 export {
     useFetchFeedPosts,
+    useFetchGroup,
     useFetchPost,
     useFetchUserData,
     useFetchUserProfilePosts,
---
+++ hooks/fetch-hooks/useFetchGroup.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { getData } from "../../utils/requests";
+
+export default function useFetchGroup(_id: string) {
+    return useQuery(['fetchGroup'], async () => {
+        const group = await getData({
+            uri: `get-group/${_id}`,
+        }).then(res => {
+            const { group } = res;
+            return group;
+        }).catch(err => {
+            console.log(`There was an error fetching a group: ${err.message}`);
+            return {
+                isError: true,
+                message: 'There was an error fetching a group',
+            };
+        });
+
+        return group;
+    }, {
+        refetchIntervalInBackground: true,
+        refetchInterval: 30000,
+        refetchOnMount: true,
+        staleTime: 30000,
+    });
+}
