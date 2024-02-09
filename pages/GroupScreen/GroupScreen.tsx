9686214fc26c7448802b39ed9c98a0febb08e62a
Simeon Ikudabo <simeonikudabo@Simeons-Air.lan>
2024-02-09 18:24:10 -0500

    Adding the tabs for the group screen.

--- pages/GroupScreen/GroupScreen.tsx
+++ pages/GroupScreen/GroupScreen.tsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import {
+    Tabs,
+    TabsProvider,
+    TabScreen,
+} from 'react-native-paper-tabs';
+import { useUser } from '../../hooks/storage-hooks';
 import { useFetchGroup } from '../../hooks/fetch-hooks';
 import { GeoCitiesAvatar, GeoCitiesBodyText, LoadingIndicator, colors } from '../../components';
 
@@ -9,8 +16,11 @@ type GroupScreenProps = {
 
 type GroupScreenDisplayLayerProps = {
     avatarUri: string;
+    currentIndex: number;
     description: string;
     groupName: string;
+    handleChangeIndex: (index: number) => void;
+    isCreator: boolean;
     isLoading: boolean;
 }
 
@@ -23,8 +33,11 @@ export default function GroupScreen({
 
 function GroupScreen_DisplayLayer({
     avatarUri,
+    currentIndex,
     description,
     groupName,
+    handleChangeIndex,
+    isCreator,
     isLoading,
 }: GroupScreenDisplayLayerProps) {
 
@@ -45,6 +58,25 @@ function GroupScreen_DisplayLayer({
                     <View style={styles.descriptionContainer}>
                         <GeoCitiesBodyText color={colors.white} fontSize={12} fontWeight='normal' text={description} />
                     </View>
+                    <View style={styles.tabsSectionContainer}>
+                        <TabsProvider defaultIndex={0} onChangeIndex={handleChangeIndex}>
+                            <Tabs style={styles.tabsStyle} tabLabelStyle={styles.tabLabel} disableSwipe>
+                                <TabScreen icon="mail" label="Posts">
+                                    <View style={{ alignItems: 'center',  flex: 1, height: 500, paddingTop: 300 }}>
+                                        <GeoCitiesBodyText color={colors.white} fontSize={44} text="Posts" />
+                                    </View>
+                                </TabScreen>
+                                <TabScreen icon="information" label="About">
+                                    <GeoCitiesBodyText color={colors.white} fontSize={44} text="About" />
+                                </TabScreen>
+                                {isCreator && (
+                                    <TabScreen icon="cog" label="Settings">
+                                        <GeoCitiesBodyText color={colors.white} fontSize={44} text="About" />
+                                    </TabScreen>
+                                )}
+                            </Tabs>
+                        </TabsProvider>
+                    </View>
                 </ScrollView>
             </SafeAreaView>
         </View>
@@ -53,18 +85,30 @@ function GroupScreen_DisplayLayer({
 
 function useDataLayer({ navigation, route }: GroupScreenProps) {
     const { _id } = route.params.group;
+    const { user } = useUser();
+    const { _id: userId } = user;
     const { data: group, isLoading } = useFetchGroup(_id);
-    const { avatar, description, groupName } = !isLoading ? group : { 
+    const { avatar, creator, description, groupName } = !isLoading ? group : { 
         avatar: '',
+        creator: '',
         description: '',
         groupName: '',
     };
+    const isCreator = creator === userId;
     const avatarUri = `${process.env.EXPO_PUBLIC_API_BASE_URI}get-photo/${avatar}`;
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    function handleChangeIndex(index: number) {
+        setCurrentIndex(index);
+    }
     
     return {
         avatarUri,
+        currentIndex,
         description,
         groupName,
+        handleChangeIndex,
+        isCreator,
         isLoading,
     };
 };
@@ -84,6 +128,8 @@ const styles = StyleSheet.create({
         width: '100%',
     },
     descriptionContainer: {
+        paddingLeft: 10,
+        paddingRight: 10,
         paddingTop: 20,
         width: '100%',
     },
@@ -91,5 +137,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         paddingTop: 20,
+    },
+    tabsSectionContainer: {
+        paddingTop: 30,
+    },
+    tabLabel: {
+        color: colors.white,
+        fontFamily: 'Montserrat_700Bold',
+    },
+    tabsStyle: {
+        color: colors.white,
+        width: '100%',
     },
 });
