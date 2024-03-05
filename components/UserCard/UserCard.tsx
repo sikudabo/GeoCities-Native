0b58520fd182493c247cf24f01c2b3eb14163eea
Simeon Ikudabo <simeonikudabo@Simeons-Air.lan>
2024-03-05 17:17:00 -0500

    Added functionality to view a users' profile from the creator card in the about section of a group.

--- components/UserCard/UserCard.tsx
+++ components/UserCard/UserCard.tsx
@@ -1,12 +1,14 @@
 import { StyleSheet, View } from 'react-native';
 import truncate from 'lodash/truncate';
 import { Surface } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 import GeoCitiesAvatar from '../GeoCitiesAvatar';
 import GeoCitiesBodyText from '../GeoCitiesBodyText';
 import GeoCitiesButton from '../GeoCitiesButton';
 import { colors } from '../colors';
 import { UserType } from '../../typings';
 import { birthdayToYears } from '../../utils/helpers';
+import { useUser } from '../../hooks/storage-hooks';
 
 type UserCardProps = {
     user: UserType;
@@ -16,6 +18,7 @@ type UserCardDisplayLayerProps = {
     avatarPath: string;
     cityStatePath: string;
     fullName: string;
+    handleViewProfilePress: () => void;
     userAge: number;
 }
 
@@ -27,6 +30,7 @@ function UserCard_DisplayLayer({
     avatarPath,
     cityStatePath,
     fullName,
+    handleViewProfilePress,
     userAge,
 }: UserCardDisplayLayerProps) {
     return (
@@ -44,23 +48,32 @@ function UserCard_DisplayLayer({
                 <GeoCitiesBodyText color={colors.white} fontSize={16} fontWeight={500} text={cityStatePath} />
             </View>
             <View style={styles.viewButtonContainer}>
-                <GeoCitiesButton buttonColor={colors.salmonPink} text="View Profile" />
+                <GeoCitiesButton buttonColor={colors.salmonPink} onPress={handleViewProfilePress} text="View Profile" />
             </View>
         </Surface>
     );
 }
 
 function useDataLayer(user: UserType) {
-    const { avatar, dob, firstName, lastName, locationCity, locationState } = user;
+    const { avatar, dob, firstName, _id, lastName, locationCity, locationState } = user;
+    const { user: currentUser } = useUser();
+    const { _id: currentUserId } = currentUser;
     const avatarPath = `${process.env.EXPO_PUBLIC_API_BASE_URI}get-photo/${avatar}`;
     const cityStatePath = `${locationCity}, ${locationState}`;
     const fullName = firstName + ' ' + lastName;
     const userAge = birthdayToYears(dob);
+    const navigation: any = useNavigation();
+
+    function handleViewProfilePress() {
+      navigation.navigate('Profile', { isVisitor: _id !== currentUserId, userId: _id !== currentUserId ? _id : undefined });
+      return;  
+    }
 
     return {
         avatarPath,
         cityStatePath,
         fullName: truncate(fullName, { length: 50 }),
+        handleViewProfilePress,
         userAge,
     };
 }
